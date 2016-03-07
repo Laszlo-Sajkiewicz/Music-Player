f71cb9c687b7cb22283ecf3be171689fece60bca
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2016-03-07 12:28:05 +0100

    MVC

--- Music-Player/Controller/music.ctrl.php
+++ Music-Player/Controller/music.ctrl.php
@@ -1,12 +1,49 @@
 <?php
-  require_once 'Model/Scanner.model.php';
+ require_once('Model/Music.model.php');
+ require_once('Model/MusicDAO.class.php');
 
-  $tab = array();
-  $directory = 'Data/';
+$config = parse_ini_file('config/config.ini');
+$jukebox = new MusicDAO($config['database_path']);
 
-  $scanner = new Scanner($directory);
+function loadMusicDB($first, $last){
+        $config = parse_ini_file('../config/config.ini');
+        $mus = new MusicDAO($config['database_path']);
+        $res = array();
+        for ($i=$first; $i < $last; $i++) {
+            $a_res[$i] = $mus->get($i);
+        }
+        return $res;
+    }
 
-  $tab['listMusic'] = $scanner->scanAll();
+        $data['musicList'] = "";
+        $data['first'] = 1;
+        $data['last'] = 10;
 
-  include_once 'View/music.view.php';
+        if (isset($_GET['mus'])){
+            $data['musicList'] += "<audio src=\"http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/M3104/TP02/data/mp3/" . ($_GET['mus']) . "\" controls autoplay></audio> \n";
+        }
+        if (isset($_GET['first'])){
+            $data['first'] = $_GET['first'];
+        }
+        if (isset($_GET['last'])){
+            $data['last'] = $_GET['last'];
+        }
+        $a_musique = loadMusicDB($data['first'], $data['last']);
+        $data['musicList'] += "<div class='music'>";
+        foreach ($a_musique as $key => $value) {
+            $data['musicList'] += "<a href=\"main.ctrl.php?mus=" . ($value->getMp3()) . "&first=" . ($data['first']) . "&last=" . ($data['last']) . "\">";
+            $data['musicList'] += "<img src=\"http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/M3104/TP02/data/img/" . ($value->getCover()) . "\" alt=\"" . ($value->getTitle()) . "\" /> \n";
+            $data['musicList'] += "</a>";
+        }
+        $data['musicList'] += "</div>";
+
+        include_once('../view/main.view.php');
+//  $tab = array();
+//  $directory = 'Data/';
+
+//  $scanner = new Scanner($directory);
+
+//  $tab['listMusic'] = $scanner->scanAll();
+
+  include_once '../View/music.view.php';
 ?>
--- Music-Player/Model/Scanner.model.php
+++ Music-Player/Model/Scanner.model.php
@@ -1,29 +1,33 @@
 <?php
   include_once 'Model/Music.model.php';
-
   /**
    * @author Laszlo Sajkiewicz
    */
   class Scanner
   {
     public $directory;
-
     function __construct($directory)
     {
       $this->directory = $directory;
     }
-
     function scanMusic() {
       return glob('' . $this->directory . '*.{mp3}', GLOB_BRACE);
     }
-
     function scanAll() {
       $tab = array();
       foreach ($this->scanMusic() as $key => $value) {
-        $tab[] = new Music($this->directory . basename($value, ".mp3").'.jpeg',
-                           $this->directory . basename($value, ".mp3").'.mp3');
+    //    $tab[] = new Music($this->directory . basename($value, ".mp3").'.jpeg',
+      //                     $this->directory . basename($value, ".mp3").'.mp3');
+      $tab[] = new Music( $this->id = $id,
+           $this->author = $author,
+           $this->title = $title,
+           $this->cover = $cover,
+           $this->mp3 = $mp3,
+           $this->category = $category)
       }
       return $tab;
     }
-  }
+
+    
+
 ?>

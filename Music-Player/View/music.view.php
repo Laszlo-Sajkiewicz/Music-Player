7d3e7cd2f381470e77ea4e4bdfe73495e2fcd10f
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2016-03-14 11:35:40 +0100

    add svg title

--- Music-Player/View/music.view.php
+++ Music-Player/View/music.view.php
@@ -6,8 +6,44 @@
     <title>Music</title>
   </head>
   <body>
-    <h1>Music player</h1><h2>Audio player</h2>
 
+
+<svg viewBox="0 0 800 600">
+  <symbol id="s-text" >
+
+    <text text-anchor="middle"
+          x="50%"
+          y="35%"
+          class="text--line"
+          >
+       Music
+    </text>
+    <text text-anchor="middle"
+          x="50%"
+          y="68%"
+          class="text--line2"
+          >
+      Player
+    </text>
+
+  </symbol>
+
+  <g class="g-ants">
+    <use xlink:href="#s-text"
+      class="text-copy"></use>
+    <use xlink:href="#s-text"
+      class="text-copy"></use>
+    <use xlink:href="#s-text"
+      class="text-copy"></use>
+    <use xlink:href="#s-text"
+      class="text-copy"></use>
+    <use xlink:href="#s-text"
+      class="text-copy"></use>
+  </g>
+
+
+</svg>
+<hr>
     <?php
       foreach ($tab['listMusic'] as $key => $value) {
     ?>
